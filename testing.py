7fc0490ea41fe9b6e9f8b2daabaac26cd2be0238
adrian <sc19acyl@leeds.ac.uk>
2020-05-29 00:54:40 +0800

    initial commit/completed joint distribution func

---
+++ testing.py
@@ -0,0 +1,9 @@
+people = {
+  'Harry': {'name': 'Harry', 'mother': 'Lily', 'father': 'James', 'trait': None},
+  'James': {'name': 'James', 'mother': None, 'father': None, 'trait': True},
+  'Lily': {'name': 'Lily', 'mother': None, 'father': None, 'trait': False}
+}
+
+one_gene = {'Harry'}
+
+print(people['Harry']['mother'])
