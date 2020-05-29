54d4f64d778b362e632a4be58c0d47cea608d8f8
adrian <sc19acyl@leeds.ac.uk>
2020-05-29 17:50:15 +0800

    deleted testing.py

--- testing.py
+++
@@ -1,9 +0,0 @@
-people = {
-  'Harry': {'name': 'Harry', 'mother': 'Lily', 'father': 'James', 'trait': None},
-  'James': {'name': 'James', 'mother': None, 'father': None, 'trait': True},
-  'Lily': {'name': 'Lily', 'mother': None, 'father': None, 'trait': False}
-}
-
-one_gene = {'Harry'}
-
-print(people['Harry']['mother'])
